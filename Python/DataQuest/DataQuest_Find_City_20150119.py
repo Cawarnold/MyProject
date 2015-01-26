3025b096dd65e9835befa5f16ff2af724c52565e
Cawarnold <christian@thisisglow.com>
2015-01-26 13:53:49 +0000

    put changes in wrong file

--- Python/DataQuest/DataQuest_Find_City_20150119.py
+++ Python/DataQuest/DataQuest_Find_City_20150119.py
@@ -1042,30 +1042,6 @@ for item in full_data:
         cities.append(city)
 print(cities)
 
-#### Testing_if_items_are_in_list ####
-
-# We can check if values are in lists using the in statement.
-the_list = [10,60,-5,8]
-
-# This is True because 10 is in the_list
-print(10 in the_list)
-
-# This is True because -5 is in the_list
-print(-5 in the_list)
-
-# This is False because 9 isn't in the_list
-print(9 in the_list)
-
-# We can assign the results of an in statement to a variable.
-# Just like any other boolean.
-a = 7 in the_list
-
-list2 = [8, 5.6, 70, 800]
-
-c = 9 in list2
-d = 8 in list2
-e = -1 in list2
-
 
 
 
