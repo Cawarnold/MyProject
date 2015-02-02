f8a2e3dcac808e51815db576ffa0627974c041e6
Cawarnold <christian@thisisglow.com>
2015-02-02 18:16:12 +0000

    Name of Us Congress

---
+++ Python/DataQuest/DataQuest_Name_of_US_Congresspersons_20150202.py
@@ -0,0 +1,29 @@
+## DataQuest_Name_of_US_Congresspersons_20150202
+
+url = ("https://dataquest.io")
+
+# You can also use ctrl+alt+r to run code. Click on the instructions panel, then type ? to see all the hotkeys.
+
+#####################################
+
+# Basics: What should you name your kid if you want them to be a US Congressperson?
+
+# Learn about the enumerate function, list comprehensions, try/except blocks, and the None type while finding the most common names for US Congressman/Congresswomen.
+
+#####################################
+
+Overview of Useful code:
+
+
+#####################################
+
+#### SUMMARY OF USEFUL CODE ####
+
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### FULL SET OF INSTRUCTIONS ####
