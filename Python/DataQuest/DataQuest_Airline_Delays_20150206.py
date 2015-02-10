8a5ce542f01dadcfb85408c155bb7881ece9ddb6
Cawarnold <christian@thisisglow.com>
2015-02-10 11:18:34 +0000

    master: clean up airline delays

--- Python/DataQuest/DataQuest_Airline_Delays_20150206.py
+++ Python/DataQuest/DataQuest_Airline_Delays_20150206.py
@@ -8,20 +8,236 @@ url = ("https://dataquest.io")
 
 # Chapter 7
 
-# Basics: Which airline is delayed the most?
+# Basics: Which airline is delayed the most? (Finding_average_delay_for_each_carrier)
 
 # Learn more about indexing, keyword arguments, and the break keyword while finding out which airlines have the most delays.
 
 #####################################
 
 Overview of Useful code:
-
+Writing a _while loop. (Writing_a_while_loop)
+Stoping a _while loop _with a _break clause. (Using_the_break_keyword)
+Define function that finds a column number _from a name. (Finding_a_column_number_from_a_name)
+Negative indexes get values _from the end of a _list. (Using_negative_indexing)
+Negative indexes, up to end _or beginning. (Indexing_up_to_the_end_or_from_the_beginning)
+Using the column_number_from_name funciton _and a _list comprehension. (Finding_the_percentage_of_delayed_flights)
+Using function _and _for loop. (Finding_the_average_delay_time)
+Function that takes column name _and returns column _sum. (Making_a_function_to_calculate_the_delay)
+Creating functions _with multiple arguments. (Named_arguments_to_functions)
+Writing functions _with optional arguments. (Optional_arguments_to_a_function)
+Write function find_average_delay that takes a carrier name. (Finding_delay_by_carrier)
+Create a _dict of delays by carrier. (Finding_average_delay_for_each_carrier)
 
 #####################################
 
 #### SUMMARY OF USEFUL CODE ####
 
+#### Writing_a_while_loop ####
 
+# The loop will keep running as long as the condition specified is True.
+
+x = 2
+while x == 2:
+    print x
+
+
+#### Using_the_break_keyword ####
+
+# The break keyword can be used to stop a loop early.
+# This can be useful in for loops when something happens inside the loop that makes us need to end it early.
+
+dog_available = False
+desired_dog = "Great Dane"
+available_dogs = ["Labrador", "Poodle", "Sheepdog", "Great Dane", "Pomeranian"]
+for dog in available_dogs:
+    if dog == desired_dog:
+        dog_available = True
+        break
+
+
+#### Finding_a_column_number_from_a_name ####
+
+## Official answer
+def column_number_from_name(column_name):
+    column_number = None
+    for i, column in enumerate(column_names):
+        if column == column_name:
+            column_number = i
+    return column_number
+
+
+#### Using_negative_indexing ####
+
+# We can index lists with negative values.
+# Positive indexes start getting values from the beginning of a list, but negative indexes get values from the end of a list.
+print(flight_delays[-1])       # Prints the last row in flight_delays
+
+# Prints the third to last and second to last rows in flight_delays (remember that slicing only goes up to but not including the second number)
+print(flight_delays[-3:-1])    # Prints the rows at index -3 and -2
+
+
+#### Indexing_up_to_the_end_or_from_the_beginning ####
+
+# Assign the first 10 rows of flight_delays to first_ten_rows.
+# Assign the last 10 rows of flight_delays to last_ten_rows.
+first_ten_rows = flight_delays[:10]
+last_ten_rows = flight_delays[-10:]
+
+
+#### Finding_the_percentage_of_delayed_flights ####
+
+# We can now find the percentage of flights that are delayed.
+
+## USING the FUNCTION: column_number_from_name
+
+# Get the column number of the arr_flight column
+# This column counts the total number of arriving flights for a carrier in a given airport
+arr_flights_column = column_number_from_name("arr_flights")
+
+# Extract all of the values in the column using a list comprehension
+# We need to convert the values to float because they are strings initially
+arr_flights = [float(row[arr_flights_column]) for row in flight_delays]
+
+# Now we can use the sum() function to add together all of the values.
+total_arriving_flights = sum(arr_flights)
+
+# Sum together the values in the "arr_del15" column. 
+# This is the total number of arriving flights in each airport that were delayed more than 15 minutes.
+arr_del15_column = column_number_from_name("arr_del15")
+arr_del15 = [float(row[arr_del15_column]) for row in flight_delays]
+total_arr_del15 = sum(arr_del15)
+# Then, divide the number of delayed flights by total_arriving_flights. 
+# Assign the result to delayed_percentage.
+
+delayed_percentage = (total_arr_del15/total_arriving_flights)
+print(delayed_percentage)
+
+#### Finding_the_average_delay_time ####
+
+# The total amount of time (in minutes) that planes for a given carrier were delayed at a given airport is in "arr_delay" column.
+## USING the FUNCTION: column_number_from_name
+
+average_delay_time = None
+arr_delays = []
+arr_del15 = []
+for i,row in enumerate(flight_delays):
+    arr_delays.append(int(row[column_number_from_name("arr_delay")]))
+    arr_del15.append(int(row[column_number_from_name("arr_del15")]))
+total_arr_delays = sum(arr_delays)
+total_arr_del15 = sum(arr_del15)
+average_delay_time = total_arr_delays / total_arr_del15
+print(average_delay_time)
+
+
+#### Making_a_function_to_calculate_the_delay ####
+
+# Make a function that takes a column name as input, and returns the column sum.
+## USING the FUNCTION: column_number_from_name
+
+def calc_sum_of(column_name):
+    list_of_values = []
+    for i,row in enumerate(flight_delays):
+        list_of_values.append(int(row[column_number_from_name(column_name)]))
+    total = sum(list_of_values)
+    return total
+
+average_weather_delay_time = calc_sum_of("weather_delay") / calc_sum_of("arr_del15")
+print(average_weather_delay_time)
+
+
+#### Named_arguments_to_functions ####
+
+# We can use named keyword arguments to pass input to a function.
+def divide(x, y):
+    return x/y
+# Use positional arguments, which will implicitly pass 10 to x and 5 to y.
+print(divide(10,5))
+> 2
+# Use named arguments, which will pass the values to the named variable.
+print(divide(y=10, x=5))
+> 0.5
+
+#### Optional_arguments_to_a_function ####
+
+# We can also specify default values in a function definition that make the arguments optional.
+# You can't have an optional argument before a non-optional argument, just like named keywords.
+def multiply(a, b=2, c=1):
+    return a * b * c
+
+print(multiply(5))          # This will multiply 5 * 2 * 1
+print(multiply(a=6, c=4))   # This will multiply 6 * 2 * 4
+
+
+#### Finding_delay_by_carrier ####
+
+# Now that we know about optional and keyword arguments, 
+# let's make a function that can find the average delay for a given airport.
+## USING the FUNCTIONS: column_number_from_name, calc_sum_of
+
+def find_average_delay(carrier_name=None):
+    if carrier_name is None:
+        arr_delay = calc_sum_of("arr_delay") 
+        arr_del15 = calc_sum_of("arr_del15")
+        average_delay_time = arr_delay / arr_del15
+    else:
+        arr_delay_list = []
+        arr_del15_list = []
+        for i, row in enumerate(flight_delays):
+            if row[column_number_from_name("carrier")] == carrier_name:
+                arr_delay_list.append(int(row[column_number_from_name("arr_delay")]))
+                arr_del15_list.append(int(row[column_number_from_name("arr_del15")]))
+                arr_delay = sum(arr_delay_list)
+                arr_del15 = sum(arr_del15_list)
+                average_delay_time = arr_delay / arr_del15
+    return average_delay_time
+
+average_delay_time = (find_average_delay())
+american_airlines_average_delay_time = (find_average_delay(carrier_name = "AA"))
+print(average_delay_time)                        # Prints average delay for all airports
+print(american_airlines_average_delay_time)      # Prints average delay for specific airport
+
+## Answer
+
+def find_average_delay(carrier_name=None):
+    total_delayed_flights = 0
+    total_delay_time = 0
+    delay_time_column = column_number_from_name("arr_delay")
+    delay_number_column = column_number_from_name("arr_del15")
+    carrier_column = column_number_from_name("carrier")
+    for row in flight_delays:
+        if carrier_name is None or row[carrier_column] == carrier_name:
+            total_delayed_flights += float(row[delay_number_column])
+            total_delay_time += float(row[delay_time_column])
+    return total_delay_time / total_delayed_flights
+
+average_delay_time = find_average_delay()
+american_airlines_average_delay_time = find_average_delay("AA")
+
+
+#### Finding_average_delay_for_each_carrier ####
+
+# Now that we have a function to find the delay for a given carrier, we can find the delays for each carrier.
+## USING the FUNCTIONS: column_number_from_name, find_average_delay
+
+# Create a list of the unique carrier names.
+# Then call the find_average_delay function for each carrier.
+# Set the carriers as keys in the delays_by_carrier dictionary, 
+# with the values being the average delay times.
+
+carrier_names_list = []
+unique_carrier_names = set()
+unique_carrier_names_list = []
+for row in flight_delays:
+    carrier_names_list.append(row[column_number_from_name("carrier")])
+unique_carrier_names = set(carrier_names_list)
+unique_carrier_names_list = list(unique_carrier_names)
+print(unique_carrier_names_list)
+
+delays_by_carrier = {}
+for item in unique_carrier_names_list:
+    delays_by_carrier[item] = find_average_delay(item)
+
+print(delays_by_carrier)
 
 
 ########################################################################################################################
