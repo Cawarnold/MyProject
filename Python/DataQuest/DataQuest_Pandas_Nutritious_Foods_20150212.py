2b4a6394b3811519adacc6917398eac12bfd1916
Cawarnold <christian@thisisglow.com>
2015-02-12 14:11:45 +0000

    Pandas nutrition example

---
+++ Python/DataQuest/DataQuest_Pandas_Nutritious_Foods_20150212.py
@@ -0,0 +1,30 @@
+## DataQuest_Pandas_Nutritious_Foods_20150212
+
+url = ("https://dataquest.io")
+
+# You can also use ctrl+alt+r to run code. Click on the instructions panel, then type ? to see all the hotkeys.
+
+#####################################
+
+# Chapter 8
+
+# Basics: Finding the most (and least) nutritious foods
+
+# Learn about pandas and vector operations while analyzing US Department of Agriculture (USDA) data to find the most nutritious foods.	
+
+#####################################
+
+Overview of Useful code:
+
+
+#####################################
+
+#### SUMMARY OF USEFUL CODE ####
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### FULL SET OF INSTRUCTIONS ####
