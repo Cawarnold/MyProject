c7786b39000a70cf0bbd64298d9238f256ba6f10
Cawarnold <christian@thisisglow.com>
2015-02-18 18:20:09 +0000

    CIA data, for structures and unicode

---
+++ Python/DataQuest/DataQuest_ComputerScience_CIAdocuments_StoringVariablesUnicode_20150218.py
@@ -0,0 +1,31 @@
+## DataQuest_ComputerScience_CIAdocuments_StoringVariablesUnicode_20150218
+
+url = ("https://dataquest.io")
+
+# You can also use ctrl+alt+r to run code. Click on the instructions panel, then type ? to see all the hotkeys.
+
+#####################################
+
+# Chapter 12
+
+# Basics: Analyze CIA documents
+
+# Learn more about how computers store variables and unicode while analyzing excerpts from CIA reports.	
+
+
+#####################################
+
+Overview of Useful code:
+
+
+#####################################
+
+#### SUMMARY OF USEFUL CODE ####
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### FULL SET OF INSTRUCTIONS ####
