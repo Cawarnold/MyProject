9dba60b213144ae38d750a20d46005cd0683bff9
Cawarnold <christian@thisisglow.com>
2015-02-26 18:11:46 +0000

    start stats section, titanic data

---
+++ Python/DataQuest/DataQuest_Statistics_Titanic_BasicStats_20150226.py
@@ -0,0 +1,30 @@
+## DataQuest_Statistics_Titanic_BasicStats_20150226
+
+url = ("https://dataquest.io")
+
+# You can also use ctrl+alt+r to run code. Click on the instructions panel, then type ? to see all the hotkeys.
+
+#####################################
+
+# Chapter 12
+
+# Basics: Looking at Titanic survival data
+
+# Learn the basics of statistics while looking at survival data from the Titanic.
+
+#####################################
+
+Overview of Useful code:
+
+
+#####################################
+
+#### SUMMARY OF USEFUL CODE ####
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### FULL SET OF INSTRUCTIONS ####
