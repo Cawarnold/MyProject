c9759652c0f807dd0bf4876343d64b0ca5109545
Cawarnold <christian@thisisglow.com>
2015-02-26 18:38:14 +0000

    beginning

--- Python/DataQuest/DataQuest_Statistics_Titanic_BasicStats_20150226.py
+++ Python/DataQuest/DataQuest_Statistics_Titanic_BasicStats_20150226.py
@@ -28,3 +28,23 @@ Overview of Useful code:
 ########################################################################################################################
 
 #### FULL SET OF INSTRUCTIONS ####
+
+#### Equal_interval_scales ####
+
+# Statistics is, at the core, about counting and measuring.
+# In order to do both effectively, we have to define scales on which we can count.
+# One type of scale is called equal interval.
+# Think of the speed of a car. 5 miles per hour is 5 miles per hour, no matter what the current speed is.
+# The difference between 60 and 55 miles per hour will always equal the difference between 10 and 5 miles per hour in real-world terms.
+# Another type of scale is a logarithmic scale.
+# The difference between a 6 and a 5 on the Richter scale is more than the difference between a 4 and 5.
+# This is because each number on the Richter scale means that the earthquake had 10 times the shaking amplitude of the previous number.
+# So, a 6 is 10 times more powerful (technically, powerful is the wrong term, but it makes explaining easier) than a 5,
+# which is 10 times more powerful than a 4. A 6 is 100 times as powerful as a 4.
+# We can calculate the mean of values on an equal interval scale by adding up all the values and dividing by the number of values.
+# We could do the same for values on a non-equal interval scale, but the results wouldn't be meaningful, 
+# because of the differences between units.
+
+
+
+
