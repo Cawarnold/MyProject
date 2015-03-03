6497e1d105620f7fbfa537b8c697bd83921cc28f
Cawarnold <christian@thisisglow.com>
2015-03-03 15:49:35 +0000

    Stats, WineQuality, Linear Regression

---
+++ Python/DataQuest/DataQuest_Statistics_WineQuality_LinRegression_20150303.py
@@ -0,0 +1,71 @@
+## DataQuest_Statistics_WineQuality_LinRegression_20150303
+
+url = ("https://dataquest.io")
+
+# You can also use ctrl+alt+r to run code. Click on the instructions panel, then type ? to see all the hotkeys.
+
+#####################################
+
+# Chapter 15
+
+# Basics: Predicting wine quality
+
+# Learn about linear regression while predicting how highly experts would rate white wines.
+
+
+#####################################
+
+Overview of Useful code:
+
+
+#####################################
+
+#### SUMMARY OF USEFUL CODE ####
+
+# we have some data on wine quality
+
+# Each row is a different wine
+# Each Column is an attribute of that wine
+
+# Columns include: density, ph, alcohol, quality(rating_by_expert-1to10)
+
+# goint to try to predict the quality using the different columns here
+## Linear Regression, allows us to compare two sets of values, 
+## train an algoritm to predict new values.
+
+# eg. use density, feed in density data. feed in quality. pedict quality based on density.
+
+#### Drawing_Lines ####
+
+#Before we get started with linear regression, let's take a look at how to draw lines.
+# The simplest possible line is y=xy=xy=x. 
+# This means that the value of a point on the y-axis is the same as the corresponding value on the x-axis.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
