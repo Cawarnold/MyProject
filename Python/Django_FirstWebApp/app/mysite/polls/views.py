c13892848661465cde006d67a65cc35969764871
Cawarnold <christian@thisisglow.com>
2015-03-18 18:09:49 +0000

    generic views

--- Python/Django_FirstWebApp/app/mysite/polls/views.py
+++ Python/Django_FirstWebApp/app/mysite/polls/views.py
@@ -1,29 +1,27 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
-# http://127.0.0.1:8000/polls/
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    #render() : https://docs.djangoproject.com/en/1.7/topics/http/shortcuts/#django.shortcuts.render
-    return render(request, template_name, context)
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-# http://127.0.0.1:8000/polls/1/
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-# http://127.0.0.1:8000/polls/1/results/
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 # http://127.0.0.1:8000/polls/1/vote/
 def vote(request, question_id):
@@ -44,64 +42,4 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-
-
-
-
-#####################################################
-################## No Longer Used ###################
-#####################################################
-#'''
-#Django provides a shortcut for rendering an Http response.
-#
-#from django.shortcuts import render
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    context = {'latest_question_list': latest_question_list}
-#    template_name = 'polls/index.html'
-#    return render(request, template_name, context)
-#
-#INSTEAD OF:
-#
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
-#    context = RequestContext(request, {
-#        'latest_question_list': latest_question_list,
-#    })
-#    return HttpResponse(template.render(context))
-#
-######## 
-#It is a very common idiom to use get() and raise Http404 if the object doesnt exist.
-#
-#  Django provides a shortcut. 
-#from django.shortcuts import get_object_or_404
-#def detail(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'polls/detail.html', {'question': question})
-#
-#INSTEAD OF:
-#
-#def detail(request, question_id):
-#    try:
-#        question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404("Question does not exist")
-#    return render(request, 'polls/detail.html', {'question': question})
-#
-#########
-# This change uses the render function in django.shortcuts
-#
-#def results(request, question_id):
-#    response = "You're looking at the results of question %s."
-#    return HttpResponse(response % question_id)
-#
-#INSTEAD OF:
-#
-#def results(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'polls/results.html', {'question': question})
-#
-#
-#
 
