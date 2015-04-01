85706f469ac6ff0f271f325aaa6d36fb099e56c3
Cawarnold <christian@thisisglow.com>
2015-04-01 14:21:36 +0100

    Heroku django app

---
+++ Python/Heroku_DjangoApp/mynotes/Heroku_DjangoApp_20150401.py
@@ -0,0 +1,36 @@
+## Heroku_DjangoApp_20150401
+
+# Getting Started with Django on Heroku
+## https://devcenter.heroku.com/articles/getting-started-with-django
+
+# Directory
+## C:\Users\Christian\Documents\GitHub\MyProject\Python\Heroku_DjangoApp
+
+# Today, with mobile front-ends, cloud back-ends, and fully automated management infrastructure, 
+# companies are able to launch across many countries at a very high speed with a strong, technology 
+# and analytics-enabled central infrastructure and light local teams.
+
+## http://work.thaslwanter.at/Stats/html/statsPreface.html
+## https://code.djangoproject.com/wiki/DjangoGraphviz
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### Short Notes ####
+
+
+
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+#### Full Notes ####
+
+
+
+
+############################################################################
+############################################################################
+############################################################################
