299a1dacf8914d52a0aecee047b193f4dbafd2ab
Cawarnold <christian.w.arnold@gmail.com>
2016-01-28 09:12:50 +0000

    CA_TR: Python for Everybody Michigan

---
+++ Python/Python_for_Everybody_Michigan/All_Course_Details.py
@@ -0,0 +1,83 @@
+#### Python for Everybody Specialization ####
+
+
+## Getting Started with Python
+	# From the "Python for Informatics book"
+
+# Course 1: Getting Started with Python
+# Course 2: Python Data Structures
+# Course 3: Using Python to Access Web Data
+# Course 4: Using Databases with Python
+# Capstone: Retrieving, Processing and Visualizing Data with Python
+
+
+
+
+
+
+
+#####################################################################
+#####################################################################
+			# Notes on the book -- Python for Informatics #
+#####################################################################
+#####################################################################
+
+# Python Reserved words
+
+# and 
+# as 
+# assert
+# break 
+# class 
+# continue
+# def 
+# del 	
+# elif 	
+# else 	
+# except 
+# exec 	 (not in python 3.0)
+# finally
+# for 	
+# from 
+# global
+# if 	
+# import
+# in 	
+# is 	
+# lambda
+# not 	
+# or 		
+# pass 	
+# print
+# raise
+# return
+# try
+# while
+# with
+# yield
+ 
+## Simple program to find the most common word in a text file.
+
+name = raw_input('Enter file:')
+handle = open(name, 'r')
+text = handle.read()
+words = text.split()
+counts = dict()
+for word in words:
+	counts[word] = counts.get(word,0) + 1
+bigcount = None
+bigword = None
+for word,count in counts.items():
+	if bigcount is None or count > bigcount:
+		bigword = word
+		bigcount = count
+print bigword, bigcount
+
+
+## User input
+	>>> name = raw_input('What is your name?\n')
+	Chuck
+	>>> print(name)
+	Chuck
+
+#### 20160114: now at Chapter 3.   http://do1.dr-chuck.com/py4inf/EN-us/book.pdf
---
+++ Python/Python_for_Everybody_Michigan/Basics_CA01.py
@@ -0,0 +1,14 @@
+#### Installing Python Screenshot
+
+## To run this file go to command line and type
+	# python Basics_CA01.py
+
+## It will then run the file.
+
+
+#print("Hello World")
+
+print("(not print 'hello word')")
+
+
+
