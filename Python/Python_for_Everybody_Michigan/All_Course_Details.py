15912e55ff7f8f23c55138f2e1fd1d19f8a498ce
Cawarnold <christian.w.arnold@gmail.com>
2016-04-22 17:07:38 +0100

    CA_TR: more crap

--- Python/Python_for_Everybody_Michigan/All_Course_Details.py
+++ Python/Python_for_Everybody_Michigan/All_Course_Details.py
@@ -2261,6 +2261,37 @@ while True:
 	print location
 
 
+## Equivalent but using XML
+
+import urllib
+import xml.etree.ElementTree as ET
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+    print data
+    tree = ET.fromstring(data)
+
+
+    results = tree.findall('result')
+    lat = results[0].find('geometry').find('location').find('lat').text
+    lng = results[0].find('geometry').find('location').find('lng').text
+    location = results[0].find('formatted_address').text
+
+    print 'lat',lat,'lng',lng
+    print location
+
+
+
+
 # The program takes the search string and constructs a URL with the search string 
 # as a properly encoded parameter and then uses urllib to retrieve the text 
 # from the Google geocoding API. 
@@ -2476,7 +2507,40 @@ while True:
 		print "Please enter location with valid country code."
 
 
+#### Exercise Chuck -- Extracting Data from XML
+	# From web services and XML
 
+import urllib
+import xml.etree.ElementTree as ET
+
+#url = 'http://python-data.dr-chuck.net/comments_42.xml'
+url = 'http://python-data.dr-chuck.net/comments_234596.xml'
+
+url_open = urllib.urlopen(url)
+data = url_open.read()
+print 'Retrieved',len(data),'characters'
+tree = ET.fromstring(data)
+
+#print data
+
+#comments = tree.findall('comments')
+#print comments[0].find('comment').find('count').text
+
+##  you can use an XPath selector string to look 
+## through the entire tree of XML for any tag named 'count'
+counts = tree.findall('.//count')
+
+#print type(counts) ## list
+#print type(counts[0])
+
+sums = 0
+count = 0
+for element in counts:
+	count = count + 1
+	sums = sums + int(element.text)
+
+print count
+print sums
 
 
 
--- Python/Python_for_Everybody_Michigan/Working/Scrap_Python.py
+++ Python/Python_for_Everybody_Michigan/Working/Scrap_Python.py
@@ -2,37 +2,71 @@
 
 ## http://www.pythonlearn.com/book_009.pdf
 
-# The program will use urllib to read the HTML
-# from the data files below, 
-# extract the href= vaues from the anchor tags, 
-# scan for a tag that is in a particular position 
-# from the top and follow that link, 
-# repeat the process a number of times, 
-# and report the last name you find.
 
-# Note - this code must run in Python 2.x and you must download
-# http://www.pythonlearn.com/code/BeautifulSoup.py
-# Into the same folder as this program
+
+count_manually = 100+99+86+85+84+82+82+81+78+78+78+77+76+68+67+67+65+65+63+63+61+58+54+53+51+50+50+48+47+42+41+38+38+35+34+33+32+26+24+24+15+14+13+13+11+10+7+7+4
+
+count_maually2 = 100+99+86+85+84+82+82+81+78+78+78+77+76+68+67+67+65+65+63+63+61+58+54+53+51+50+50+48+47+42+41+38+38+38+35+34+33+32+26+24+24+15+14+13+13+11+10+7+7+4
 
 import urllib
-import re
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-url = "http://python-data.dr-chuck.net/known_by_Fox.html"
+#url = 'http://python-data.dr-chuck.net/comments_42.xml'
+url = 'http://python-data.dr-chuck.net/comments_234596.xml'
 
-repeat = 0
-while repeat < 7:
-	print url
-	html = urllib.urlopen(url).read()
-	soup = BeautifulSoup(html, "html.parser")
-	tags = soup('a')
-	list_urls = []
-	for tag in tags:
-		list_urls.append(tag.get('href', None))
-	url = list_urls[17]
-	repeat = repeat + 1
+url_open = urllib.urlopen(url)
+data = url_open.read()
+print 'Retrieved',len(data),'characters'
+tree = ET.fromstring(data)
 
-print "Last name in sequence:", re.findall("known_by_(.+).html",url)[0]
+#print data
+
+#comments = tree.findall('comments')
+#print comments[0].find('comment').find('count').text
+
+##  you can use an XPath selector string to look 
+## through the entire tree of XML for any tag named 'count'
+counts = tree.findall('.//count')
+
+#print type(counts) ## list
+#print type(counts[0])
+
+sums = 0
+count = 0
+for element in counts:
+	count = count + 1
+	sums = sums + int(element.text)
+
+print count
+print sums
+print count_manually
+print count_maually2
+
+############
+'''
+import urllib
+import xml.etree.ElementTree as ET
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+    print data
+    tree = ET.fromstring(data)
 
 
+    results = tree.findall('result')
+    lat = results[0].find('geometry').find('location').find('lat').text
+    lng = results[0].find('geometry').find('location').find('lng').text
+    location = results[0].find('formatted_address').text
 
+    print 'lat',lat,'lng',lng
+    print location
+    '''
