35f41cdfd3229f0c433f69b4f0d8ab0d26557eae
Cawarnold <christian.w.arnold@gmail.com>
2016-06-29 23:05:31 +0100

    CA_TR: gmane exercise

--- Python/Python_for_Everybody_Michigan/All_Course_Details.py
+++ Python/Python_for_Everybody_Michigan/All_Course_Details.py
@@ -5045,8 +5045,56 @@ arwhyte-63aXycvo3TyHXe+LvDLADg@public.gmane.org
 
 
 
+###############################################################################################
+###############################################################################################
+###############################################################################################
+
+## Week 3: Spidering Modeling Email data
+
+# This week we do the first half of a project to download, process, and visualize 
+# an email corpus from the Sakai open source project from 2004-2011:
+
+	# http://mbox.dr-chuck.net/
+
+# This is a large amount of data and requires significant cleanup 
+# to make sense of the data before we visualize it.
+
+# Important: You do not have to download all of the data to complete this project. 
+# Depending on your Internet connection, downloading nearly a gigabyte of data might be impossible. 
+# All we want to do is to have you download a small subset of the data 
+# and run the steps to process the data.
+
+# Here is the software we will be using to retrieve and process the email data:
+
+	# http://www.dr-chuck.net/pythonlearn/code/gmane.zip
+
+# If you have a fast network connection with no bandwidth charge 
+# - you can download all the data. 
+# If you try to download all the data it may take well over 24 hours to pull the data. 
+# The good news is that because there are separate crawl, clean, model, and visualization steps, 
+# you can start and stop the crawl process as often as you like 
+# and run the other processes on the data which has been downloaded so far.
 
 
+#### Exercise: Loading and Modeling Mail Data
+
+# This assignment you will start the spidering of the email data 
+# and run the gmodel.py program and take some screen shots. 
+# You are welcome to look at the code and make modifications - 
+# but you can complete the assignment without making any code changes.
+
+	# Mailing List Data - Part I
+
+	# In this assignment you will download some of the mailing list data 
+	# from http://mbox.dr-chuck.net/ 
+	# and run the data cleaning / modeling process and take some screen shots.
+
+	# Don't take off points for little mistakes. 
+	# If they seem to have done the assignment give them full credit. 
+	# Feel free to make suggestions if there are small mistakes. 
+	# Please keep your comments positive and useful. 
+
+	# Sample solution: http://www.dr-chuck.net/pythonlearn/code/gmane.zip
 
 
 
