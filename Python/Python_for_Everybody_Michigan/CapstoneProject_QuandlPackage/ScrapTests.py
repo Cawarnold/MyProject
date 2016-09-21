85c458ce1fabb6ed7d9c2e207e7652112c93625e
Cawarnold <christian.w.arnold@gmail.com>
2016-09-21 16:06:25 +0100

    Update ScrapTests.py

--- Python/Python_for_Everybody_Michigan/CapstoneProject_QuandlPackage/ScrapTests.py
+++ Python/Python_for_Everybody_Michigan/CapstoneProject_QuandlPackage/ScrapTests.py
@@ -7,7 +7,7 @@
 import pyodbc
 import codecs
 
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=LDNSQLDEV03.PRACTICALLAW.COM;DATABASE=vMDB;UID=TEN\U6030064;Trusted_connection=yes')
+cnxn = pyodbc.connect('')
 
 cursor = cnxn.cursor()
 
@@ -15,7 +15,7 @@ cursor = cnxn.cursor()
 
 #### Query the DB ####
 
-cursor.execute("select top 1 * from [vMDB_Test].dbo.[RN_NormalisedRoles_ID]")
+cursor.execute("")
 row = cursor.fetchone()
 while row is not None:
 	if row > 0 : print('CONNECTION WORKS')
@@ -26,7 +26,7 @@ while row is not None:
 
 # Opens file, truncates file (with 'w+').
 
-filepath = "C:/Users/U6030064/Documents/MyThomsonReuters_20150601/Christian_MarketingProjects/CRS_ContractReportingSystem_CDB_20150827/WebsubsMigration/Websubs_20160804/WRPWebsubs_JF05_AddressBack_20160816/JF_Websubs_Data_ContactDetails_TextFile_CA01.txt"
+filepath = ""
 fhand = codecs.open(filepath, "w+", "utf-8")
 
 cursor.execute("select distinct rn.RN_ID,rn.OrgType,rn.JobClass,rn.JobType,rn.JobLevel,rn.JobFunction,rn.JobTitle from [vMDB_Test].dbo.[RN_NormalisedRoles_ID]")
