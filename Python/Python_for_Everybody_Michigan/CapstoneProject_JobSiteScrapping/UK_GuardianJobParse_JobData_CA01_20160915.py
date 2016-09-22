c0fb426bcea74a6fbd631dfaace1f528b72d7a37
Cawarnold <christian.w.arnold@gmail.com>
2016-09-22 17:17:11 +0100

    CA_TR: Job details

--- Python/Python_for_Everybody_Michigan/CapstoneProject_JobSiteScrapping/UK_GuardianJobParse_JobData_CA01_20160915.py
+++ Python/Python_for_Everybody_Michigan/CapstoneProject_JobSiteScrapping/UK_GuardianJobParse_JobData_CA01_20160915.py
@@ -30,13 +30,13 @@ cur = conn.cursor()
 #### Create Tables ####
 
 ## Drop tables if need to restart
-# cur.execute('DROP TABLE IF EXISTS JobPost_Parse_HTML ')
+# cur.execute('DROP TABLE IF EXISTS JobPost_Details ')
 
 
 ## Create tables JobPost_Parse_HTML 
-# cur.execute('''CREATE TABLE IF NOT EXISTS JobPost_Parse_HTML
-#	(jp_parse_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
-#	, jp_url_id int)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS JobPost_Details
+	(jp_parse_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+	, jp_url_id int)''')
 
 ####
 
@@ -64,8 +64,7 @@ cur = conn.cursor()
 #### Scrape HTML
 
 
-
-cur.execute('SELECT HTML FROM JobPost_HTML WHERE jp_url_id = ? ', (1, ))
+cur.execute('SELECT HTML FROM JobPost_HTML WHERE jp_url_id = ? ', (3, ))
 html = cur.fetchone()[0]
 
 #print(html[0:2000])
@@ -87,21 +86,21 @@ soup = BeautifulSoup(html, "html.parser")
 #	print(item.h1.contents[0])
 #	print(type(item.h1.contents[0]))
 
-#for item in soup.findAll('div', attrs={'class':'grid'}):
-#	for h1 in item.findAll('h1', attrs={'itemprop':'title'}):
-#		job_title = str(h1.contents[0])
-#
-#print(job_title)
+for item in soup.findAll('div', attrs={'class':'grid'}):
+	for h1 in item.findAll('h1', attrs={'itemprop':'title'}):
+		job_title = str(h1.contents[0])
+
+# print(job_title)
 
 #########################################
 #### Recruiter ####
 
-#recruiter = []
-#for item in soup.findAll('div', attrs={'class': 'grid'}):
-#	for span in item.findAll('span', attrs={'itemprop':'name'}):
-#		for line in span:
-#			recruiter.append(line)
-#print(recruiter[0])
+recruiter_list = []
+for item in soup.findAll('div', attrs={'class': 'grid'}):
+	for span in item.findAll('span', attrs={'itemprop':'name'}):
+		for line in span:
+			recruiter_list.append(line)
+recruiter = recruiter_list[0]
 
 #########################################
 #### job_details ####
@@ -114,31 +113,22 @@ for item in soup.findAll('div', attrs={'class': 'grid'}):
 	for dds in item.findAll("dd"):
 		dd_items.append(re.sub('[^A-Za-z0-9\-]+', '', dds.contents[0]))
 
-#print(dt_items)
-#print(dd_items)
-
-## Location
-
-if dt_items[1] == 'Location':
-	job_location = dd_items[1]
-
-## Salary Range
-
-if dt_items[2] == 'Salary':
-	job_salary = dd_items[2]
-	if re.match('[0-9]+[\-][0-9]+[a-zA-Z]+',job_salary):
-		salary_high = (re.split('[\-](.*?)[a-zA-Z]+',job_salary))[1]
-		salary_low = (re.split('[\-](.*?)[a-zA-Z]+',job_salary))[0]
-
-
-print(salary_high)
-print(salary_low)
-
-job_salary = (int(salary_high) + int(salary_low))/2
+#if dt_items[2] == 'Salary': job_salary = dd_items[2]
+#	if re.match('[0-9][0-9][0-9]+[\-][0-9][0-9][0-9]+[a-zA-Z]+',job_salary): # 3 or more digits
+#		salary_high = (re.split('[\-](.*?)[a-zA-Z]+',job_salary))[1]
+#		salary_low = (re.split('[\-](.*?)[a-zA-Z]+',job_salary))[0]
+#job_salary = (int(salary_high) + int(salary_low))/2
 
 
 
+if dt_items[1] == 'Location': job_location 			= dd_items[1]
+if dt_items[2] == 'Salary': job_salary 				= dd_items[2]
+if dt_items[3] == 'Posted':	job_posted 				= dd_items[3]
+if dt_items[4] == 'JobLevel': job_joblevel			= dd_items[4]
+if dt_items[5] == 'Hours': job_hours 				= dd_items[5]
+if dt_items[6] == 'Contract': job_contract 			= dd_items[6]	
+if dt_items[7] == 'ListingType': job_listingtype	= dd_items[7]	
 
-#print(job_location)
-print(job_salary)
+
+print(dt_items)
 
