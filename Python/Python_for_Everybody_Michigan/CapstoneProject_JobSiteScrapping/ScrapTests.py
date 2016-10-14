a5bf8219ed5f7f45304bc15159f0c6155742380a
Cawarnold <christian.w.arnold@gmail.com>
2016-10-14 17:22:24 +0100

    CA_TR: stupid beautiful soup not finding tags

--- Python/Python_for_Everybody_Michigan/CapstoneProject_JobSiteScrapping/ScrapTests.py
+++ Python/Python_for_Everybody_Michigan/CapstoneProject_JobSiteScrapping/ScrapTests.py
@@ -18,7 +18,6 @@ import datetime
 
 # activate Env_Python2712
 
-
 h_url = '?s=1&o4=&q=python&o1=&matchany=0&o2=893&ij=0&ds=&e=&page=1&lang=&referral=0&sortdir=desc&st=0&o3=&sortby=WebsiteDate&pagesize=10'
 
 
@@ -27,7 +26,7 @@ url_headers = { 'User-Agent': 'Mozilla/5.0' }
 
 url = base_url + str(h_url)
 r = requests.get(url, headers=url_headers)
-# print(r.url)
+print(r.url)
 # print(r.status_code)
 
 soup = BeautifulSoup(r.content, "html.parser")
@@ -40,12 +39,21 @@ for table in soup.findAll('table', attrs={'class': 'vacancyResults'}):
 		print(a.contents[0])
 		print(a.get('href'))
 		job_url = a.get('href')
+
+		try:
+			print('Trying url (job)')
+			cur.execute('''INSERT INTO JobPost_URLs (url_date, url) VALUES (?, ?,)''', 
+				(datetime.datetime.now().strftime ("%Y%m%d"), r.url, ) )
+			print('URL inserted')
+
+		except:
+			print('URL (job) already in database')
+
+
 		job_r = requests.get(job_url, headers=url_headers)
 		print(job_r.url)
 		print(job_r.status_code)
 		soup = BeautifulSoup(job_r.content, "html.parser")
-
-
 
 
 
