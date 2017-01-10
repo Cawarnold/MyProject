92b49308928a2e879613a53550d6d0e236b286b9
Cawarnold <christian.w.arnold@gmail.com>
2017-01-10 10:31:40 +0000

    CA_Home: JS updates

---
+++ JavaScript/Interview_Examples.py
@@ -0,0 +1,8 @@
+#### Interview Example ####
+
+#### Write some object oriented code which uses all 4 and explain ####
+
+## Example for a bank account:
+
+#relate an account holder to a person. this person will have a Surname, Forname, Address, Birthday
+
