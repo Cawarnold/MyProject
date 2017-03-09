41877869da29567db34474d81d56a2d06e0dd402
Christian <christian.w.arnold@gmail.com>
2017-03-09 09:29:37 +0000

    CA_Home: Pi stuff

--- RaspberryPi/Flask_App/app.py
+++ RaspberryPi/Flask_App/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+	return render_template('index.html')
+
+@app.route('/cakes')
+def cakes():
+	return 'Yummy cakes!'
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+    
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
+
+
